a3fdb1c3794b20c62d0586a5ada9d8928f856278
Nikita PB <npb.nikita@gmail.com>
2020-06-13 21:34:54 +0300

    Adds propTypes and sMovieCard component

---
+++ src/components/small-movie-card/small-movie-card.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SmallMovieCard = ({movieTitle}) => {
+  return (
+    <article className="small-movie-card catalog__movies-card">
+      <div className="small-movie-card__image">
+        <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt={movieTitle} width="280" height="175" />
+      </div>
+      <h3 className="small-movie-card__title">
+        <a className="small-movie-card__link" href="movie-page.html">{movieTitle}</a>
+      </h3>
+    </article>
+  );
+};
+
+SmallMovieCard.propTypes = {
+  movieTitle: PropTypes.string.isRequired
+};
+
+export default SmallMovieCard;
--- src/index.js
+++ src/index.js
@@ -8,7 +8,33 @@ const promoMovieData = {
   releaseDate: 2014
 };
 
+const movieListTitles = [
+  `Fantastic Beasts: The Crimes of Grindelwald`,
+  `Bohemian Rhapsody`,
+  `Macbeth`,
+  `Aviator`,
+  `We need to talk about Kevin`,
+  `What We Do in the Shadows`,
+  `Revenant`,
+  `Johnny English`,
+  `Shutter Island`,
+  `Pulp Fiction`,
+  `No Country for Old Men`,
+  `Snatch`,
+  `Moonrise Kingdom`,
+  `Seven Years in Tibet`,
+  `Midnight Special`,
+  `War of the Worlds`,
+  `Dardjeeling Limited`,
+  `Orlando`,
+  `Mindhunter`,
+  `Midnight Special`
+];
+
 ReactDOM.render(
-    <App {...promoMovieData}/>,
+    <App
+      {...promoMovieData}
+      titlesArr={movieListTitles}
+    />,
     document.getElementById(`root`)
 );
